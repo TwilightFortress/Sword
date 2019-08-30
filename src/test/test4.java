f8c8505af9e416a9c31d1167815ab8f54cc490af
yuyang <448487653@qq.com>
2019-08-30 14:40:51 +0800

    Merge branch 'master' of https://github.com/TwilightFortress/Sword
    # Conflicts:
    #	src/others/aaatest5.java

--- src/test/test4.java
+++ src/test/test4.java
@@ -1,4 +1,14 @@
 package test;
 
 public class test4 {
+    int a = 3;
+    public static void main(String[] args) {
+        test4 t = new test4();
+        t.fun();
+
+    }
+
+    public static void fun(){
+
+    }
 }

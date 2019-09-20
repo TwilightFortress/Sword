2df1db9d3955f2576c6f3ba907df01b8da33f43f
yuyang <448487653@qq.com>
2019-09-20 23:35:58 +0800

    Merge branch 'master' of https://github.com/TwilightFortress/Sword
                           # Conflicts:
                           #	src/test/test4.java

--- src/others/java基础/基础3.java
+++ src/others/java基础/基础3.java
@@ -1,9 +1,9 @@
-package test;
+package others.java基础;
 
 import java.util.HashMap;
 import java.util.Hashtable;
 
-public abstract  class test1 {
+public abstract  class 基础3 {
     public static void main(String[] args) {
         HashMap<String, String> h = new HashMap<>();
         h.put(null, "hello");
